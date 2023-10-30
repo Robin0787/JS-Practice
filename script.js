29d7f73adfc99db984b74798e71e61b72a16cc11
Robin <mohammadrobin636@gmail.com>
2023-10-30 22:00:25 +0600

    Removed

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 
 
-const arr = [2,3,5,2,4,6,7,4,2];
+const arr = [4,3,4,5,6,4,5,6,4,9,4,2,5,7];
 
-function findLargestSum(arr, target) {
-    if(arr.length > 0) {
-        const loopIteration = (arr.length - target) + 1;
+function findSumOfConsecutiveNumbers(arr, target) {
+    const arrayIteration = (arr.length - target) + 1;
     let maxSum = 0;
-    for(let i=0; i <= loopIteration; i++) {
+
+    for(let i=0; i<=arrayIteration; i++) {
         let tempSum = 0;
+
         for(let j=0; j<target; j++) {
-            tempSum += arr[i+j];
+            tempSum += arr[j];
         }
         if(tempSum > maxSum) {
             maxSum = tempSum;
         }
     }
     return maxSum;
-    } else {
-        return 'Array is empty';
-    }
 }
 
 
-const result = findLargestSum(arr, 3);
+const result = findSumOfConsecutiveNumbers(arr, 4);
 console.log(result);
